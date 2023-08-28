74711aa87589cab146cfacedfb92c99b500c4bf7
aEnchev669 <aenchev669@abv.bg>
2023-08-28 12:01:29 +0300

    Create Program.cs

---
+++ 02. SUm digits/02. SUm digits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.SUm_digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum+= int.Parse((number[i]).ToString());
+
+                
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
+
