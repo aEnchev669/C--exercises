3b8922cc9741c3212dc8c18d91ea83449ad7e4e4
aEnchev669 <aenchev669@abv.bg>
2023-09-10 14:13:11 +0300

    Create Program.cs

---
+++ 02. Odd Occurrences/02. Odd Occurrences/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ').ToArray();
+
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            foreach (var items in arr)
+            {
+                string item = items.ToLower();
+                if (map.ContainsKey(item))
+                {
+                    map[item]++;
+                }
+                else
+                {
+                    map.Add(item, 1);
+                }
+            }
+
+
+
+            foreach (var item in map)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    Console.Write($"{item.Key} ");
+
+                }
+            }
+        }
+    }
+}
