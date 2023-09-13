91cc7040592e4b6b3e5fef3dd3d8b8d49dbd0ded
aEnchev669 <aenchev669@abv.bg>
2023-09-13 15:43:44 +0300

    Create Program.cs

---
+++ 1. Sum Matrix Elements/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+
+            int rows = int.Parse(rowsNCows[0]);
+            int cols = int.Parse(rowsNCows[1].Trim());
+
+            string[,] matrix = new string[rows, cols];
+
+
+            foreach (var item in )
+            {
+
+            }
+
+
+        }
+    }
+}
