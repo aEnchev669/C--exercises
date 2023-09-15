ee743bf5a52266c76c0408f27ac211bb136e088a
aEnchev669 <aenchev669@abv.bg>
2023-09-15 09:45:38 +0300

    Create Program.cs

---
+++ 3. Elevator/3. Elevator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int people =  int.Parse(Console.ReadLine());
+          int maxCapacity = int.Parse(Console.ReadLine());
+
+            int courses = 0;
+
+
+            
+
+            while (people > 0)
+            {
+                people -= maxCapacity;
+                courses++;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}
