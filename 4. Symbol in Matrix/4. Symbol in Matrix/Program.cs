258d3292b244906c10dead512c587fd7f2bbd1ab
aEnchev669 <aenchev669@abv.bg>
2023-09-21 10:05:17 +0300

    Create Program.cs

---
+++ 4. Symbol in Matrix/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                string data = Console.ReadLine();
+
+                for (int c = 0; c < size; c++)
+                {
+                    matrix[r, c] = data[c];
+                }
+            }
+
+            char givenElement = char.Parse(Console.ReadLine());
+
+
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    if (matrix[r, c] == givenElement)
+                    {
+                        Console.WriteLine($"({r}, {c})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{givenElement} does not occur in the matrix");
+
+
+
+        }
+    }
+}
