54a8d7b30d9885cc049cdf947a09e918678f0b0a
aEnchev669 <aenchev669@abv.bg>
2023-09-22 13:32:16 +0300

    Create Program.cs

---
+++ 4. Word Filter/4. Word Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4.Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(' ')
+                .Where(w => w.Length % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", arr));
+
+        }
+    }
+}
