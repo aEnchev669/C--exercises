12bb3cfc35a0c47068f23466e1086208f76e574d
aEnchev669 <aenchev669@abv.bg>
2023-09-27 23:08:07 +0300

    C#

---
+++ 2. Vowels Count/2. Vowels Count/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            int sum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'y' || word[i] == 'o' || word[i] == 'u' || word[i] == 'e' || word[i] == 'i')
+                {
+                    sum++;
+                }
+           
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+//.Split().ToString()
