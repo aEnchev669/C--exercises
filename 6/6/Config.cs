d2c13bc5370b066289e6fa1a995509cf0ad164ec
aEnchev669 <aenchev669@abv.bg>
2024-01-16 19:47:03 +0300

    C Sharp

---
+++ 6/6/Config.cs
@@ -0,0 +1,9 @@
+﻿
+namespace _6_
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=ALEKS\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
