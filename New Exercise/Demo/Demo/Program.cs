8603343fa8b43b4a4b2492b159fc0a06b8523b08
aEnchev669 <aenchev669@abv.bg>
2024-05-24 22:05:46 +0300

    Create Program.cs

---
+++ New Exercise/Demo/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] num = {5};
+		
+			Add(num, 15);
+
+			Console.WriteLine(num[0]);
+		}
+
+		public static void Add(int[] currNum, int value)
+		{
+			currNum[0] += value;
+		}
+	}
+} 
