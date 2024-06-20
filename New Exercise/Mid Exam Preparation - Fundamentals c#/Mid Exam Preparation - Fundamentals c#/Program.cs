7dcaf8ff78631c985b658ff8d69c4bf104031fc1
aEnchev669 <aenchev669@abv.bg>
2024-06-20 23:42:30 +0300

    Create Program.cs

---
+++ New Exercise/Mid Exam Preparation - Fundamentals c#/Mid Exam Preparation - Fundamentals c#/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double food = double.Parse(Console.ReadLine()) * 1000;
+			double hay = double.Parse(Console.ReadLine()) * 1000;
+			double cover = double.Parse(Console.ReadLine()) * 1000;
+			double pigsWeight = double.Parse(Console.ReadLine()) * 1000;
+
+			for (int day = 1; day <= 30; day++)
+			{
+
+
+				food -= 300;
+
+				if (day % 2 == 0)
+				{
+					hay -= 0.05 * food;
+				}
+				if (day % 3 == 0)
+				{
+					cover -= pigsWeight / 3;
+				}
+			}
+
+			if (food <= 0 || hay <= 0 || cover <= 0)
+			{
+				Console.WriteLine("Merry must go to the pet store!");
+			}
+			else
+			{
+				Console.WriteLine($"Everything is fine! Puppy is happy! Food: {(food / 1000):f2}, Hay: {(hay / 1000):f2}, Cover: {(cover / 1000):f2}.");
+			}
+
+		}
+	}
+}
