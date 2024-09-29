4d14e882a1853582f5b042f6689f29623f614506
aEnchev669 <aenchev669@abv.bg>
2024-09-29 22:04:20 +0300

    Create Program.cs

---
+++ New Exercise/HackerRankExercises/HackerRankExercises/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HackerRankExercises
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					Console.WriteLine("FizzBuzz");
+				}
+				else if (i % 5 == 0)
+				{
+					Console.WriteLine("Buzz");
+				}
+				else if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz");
+				}
+				else
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
