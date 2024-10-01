c63bb6473b26ff1a0a736372cfefebd44deae736
aEnchev669 <aenchev669@abv.bg>
2024-10-01 23:41:53 +0300

    Stack And Queues

---
+++ New Exercise/Fashion Boutique/Fashion Boutique/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Fashion_Boutique
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+			int maxCapacity= int.Parse(Console.ReadLine());
+
+			Stack<int> stack = new Stack<int>();
+
+			input.ToList().ForEach(x => stack.Push(x));
+
+			int sum = 0;
+			int countOfRacks = 1;
+			while (stack.Count != 0)
+			{
+				if(sum + stack.Peek()  <= maxCapacity)
+				{
+					sum += stack.Pop();
+				}
+				else
+				{
+					countOfRacks++;
+					sum = 0;
+				}
+			}
+            Console.WriteLine(countOfRacks);
+        }
+	}
+}
---
+++ New Exercise/Songs Queue/Songs Queue/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Songs_Queue
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] input = Console.ReadLine().Split(", ").ToArray();
+
+
+			Queue<string> songs = new Queue<string>();
+
+			input.ToList().ForEach(x => songs.Enqueue(x));
+
+
+
+			while (songs.Count != 0)
+			{
+				string[] command = Console.ReadLine().Split().ToArray();
+
+				if (command[0] == "Play")
+				{
+					songs.Dequeue();
+				}
+				else if (command[0] == "Add")
+				{
+					string song = string.Join(" ", command.Skip(1));
+
+					if (!(songs.Any(s => s == song)))
+					{ 
+						songs.Enqueue(song);
+					}
+					else
+					{
+						Console.WriteLine($"{song} is already contained!");
+					}
+				}
+				else if (command[0] == "Show")
+				{
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+			}
+			Console.WriteLine("No more songs!");
+		}
+	}
+}
